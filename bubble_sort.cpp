be297b732eb995d55bd96505f8ead2c6d14516ac
faliss <gaal.faliss@gmail.com>
2025-07-06 22:45:48 -0500

    Started the code made the file

---
+++ bubble_sort.cpp
@@ -0,0 +1,10 @@
+// Falis Gaal 
+// Project 1 
+// July 6th 
+/* this Program creates a linlked list from user input.
+ displays it, sorts it using bubble. sort and then shows 
+ the sorted result. memory is probably cleaned up*/
+ 
+#include <iostream>
+using namespace std;
+
